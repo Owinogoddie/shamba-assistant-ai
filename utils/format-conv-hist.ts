ed25bf0f9f0996edc9af5ffc08eaebe5325959f6
Owinogoddie <owinogoddie@gmail.com>
2024-06-25 15:56:14 +0300

    removing some utils

--- utils/format-conv-hist.ts
+++ utils/format-conv-hist.ts
@@ -1,6 +1,6 @@
 export const formatConvHist=(messages:any)=>{
-    return messages.map((message,i)=>{
-        if(i%2===o){
+    return messages.map((message:any,i:any)=>{
+        if(i%2===0){
             return `Human: ${message}`
         }
         if(i%2===1){
